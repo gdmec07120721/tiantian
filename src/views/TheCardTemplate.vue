7ae1b7afb82d5f7ff0c5fa77ec1e92937fc46e01
xaobi <7041230132qq.com>
2019-07-30 16:28:37 +0800

    feat：添加卡一布局
    feat：添加卡一布局

---
+++ src/views/TheCardTemplate.vue
@@ -0,0 +1,35 @@
+<style>
+  
+</style>
+
+<template>
+  <div class="card-one">
+    <div class="card-one-name">
+      <span>asdsad</span>
+      <span>adssad</span>
+    </div>
+    <div class="card-one-info">
+      <ul>
+        <li>
+          <van-icon name="hotel-o" />
+          <span>xxxxx</span>
+        </li>
+        <li>
+          <van-icon name="phone-o" />
+          <span>xxxxx</span>
+        </li>
+      </ul>
+      <div>
+        <van-image
+          width="50px"
+          height="50px"
+          fit="cover"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+        />
+      </div>
+    </div>
+    <div class="card-one-btn">
+      <van-button type="danger" size="small">小型按钮</van-button>
+    </div>
+  </div>
+</template>

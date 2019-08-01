f987c6c08940e48e5fc7fa87c2c7b3f437fde89c
xaobi <704123013@qq.com>
2019-08-01 20:55:07 +0800

    修改
    修改

--- src/views/common/TheArticleFooter.vue
+++ src/views/common/TheArticleFooter.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="preview-footer">
     <div class="text-right">
-      <van-button v-if="showInsterBtn" type="primary" class="preview-inster-btn">插入我的名片</van-button>
+      <van-button v-if="showInsterBtn" type="primary" class="preview-inster-btn" @click="submit">插入我的名片</van-button>
     </div>
     <div class="preview-ad-benner"></div>
-    <van-button v-if="showShareBtn" type="primary" class="primary-btn">即刻分享</van-button>
+    <van-button v-if="showShareBtn" type="primary" class="primary-btn" @click="submit">即刻分享</van-button>
   </div>
 </template>
 
@@ -19,6 +19,11 @@ export default {
     showInsterBtn: {
       type: Boolean,
       default: false
+    }
+  },
+  methods: {
+    submit() {
+      this.$emit('on-submit');
     }
   }
 };

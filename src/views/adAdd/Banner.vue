3c7e5f1ae9c0016e3d8303e3a51b2b53df8ab110
xaobi <704123013@qq.com>
2019-08-06 10:11:19 +0800

    添加icon

--- src/views/adAdd/Banner.vue
+++ src/views/adAdd/Banner.vue
@@ -1,10 +1,45 @@
+<style>
+.banner-upload-wrap {text-align: center;}
+.icon-banner-upload {width: 60px; height: 56px; background: url(../../assets/icon-banner-upload); background-size: contain;}
+</style>
+
 <template>
   <div>
-    <van-cell-group>
-      <van-cell title="单元格" is-link />
-      <van-cell title="单元格" is-link value="内容" />
-      <van-cell title="单元格" is-link arrow-direction="down" value="内容" />
-    </van-cell-group>
+    <div class="banner-upload-wrap">
+      <van-uploader>
+        <span class="icon-banner-upload"></span>
+        <p>请点击此处，添加广告图片</p>
+      </van-uploader>
+    </div>
+    <div class="banner-info-wrap">
+      <ul>
+        <li>
+          <p>
+            <span class="iconfont icon-gongdanguanli-gongda"></span>
+            <span>图片点击效果</span>
+          </p>
+          <div>
+            <van-button 
+              v-for="(value, key) in click_type_config" 
+              :key="key" 
+              :type="click_type == key ? 'warning' : 'default'"
+              @click="click_type = key"
+            >
+              {{ value }}
+            </van-button>
+          </div>
+        </li>
+        <li>
+          <p>
+            <span class="iconfont icon-lianjie"></span>
+            <span>添加链接</span>
+          </p>
+          <div>
+            <van-field v-model="value" placeholder="请输入链接" />
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -15,6 +50,16 @@ export default {
     newId: {
       type: [String, Number],
       default: ''
+    }
+  },
+  data() {
+    return {
+      click_type: 0,
+      click_type_config: {
+        0: '跳转链接',
+        1: '拨打电话',
+        2: '展示海报'
+      }
     }
   }
 };

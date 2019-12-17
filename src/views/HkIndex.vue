961aaa1b6ac9ab8233d059f0f4f06090e27c8b7c
xaobi <704123013@qq.com>
2019-12-17 11:34:50 +0800

    添加获客页面代码
     添加获客页面代码

--- src/views/HkIndex.vue
+++ src/views/HkIndex.vue
@@ -3,25 +3,40 @@
     <div class="hk-index-header">
       <h3><span>精准追踪 助你成就销冠</span></h3>
     </div>
-    <marquee direction="up" onmouseover="this.stop()" onmouseout="this.start()" ontouchstart="this.stop()" ontouchend="this.start()">
-      <ul class="hk-index-marquee">
-        <li>
-          <div class="marquee-left">
-            <van-image
-              width="30px"
-              height="30px"
-              fit="cover"
-              :src="item.headimgurl || 'https://img.yzcdn.cn/vant/cat.jpeg'"
-            />
-            <span>test</span>
-          </div>
-          <div class="marquee-right"></div>
-        </li>
-      </ul>
-      <p>开发工程师</p>
-      <p>设计师</p>
-      <p>前端开发</p>
-    </marquee>
+    <div class="hk-index-qrcode">
+      <div class="hk-index-qrcode-img">
+        <van-image
+          width="125"
+          height="125"
+          src="@/assets/images/qrcode.png"
+        />
+      </div>
+      <p>长按关注天天助推</p>
+    </div>
+    <div class="hk-index-marquee">
+      <div class="hk-index-marquee-title">
+        <h3 class="">这些冠销商都在用</h3>
+      </div>
+      <marquee direction="up" onmouseover="this.stop()" onmouseout="this.start()" ontouchstart="this.stop()" ontouchend="this.start()">
+        <ul>
+          <li>
+            <div class="marquee-left">
+              <van-image
+                width="30px"
+                height="30px"
+                fit="cover"
+                :src="item.headimgurl || 'https://img.yzcdn.cn/vant/cat.jpeg'"
+              />
+              <span>test</span>
+            </div>
+            <div class="marquee-right">
+              <span>累计获客<strong class="text-primary pxy-xxs">800</strong>次</span>
+            </div>
+          </li>
+        </ul>
+      </marquee>
+    </div>
+
   </div>
 </template>
 
@@ -48,10 +63,7 @@ export default {
 
 <style lang="scss" scoped>
 .hk-index {
-  width: 100vw; 
-  min-height: 100vh; 
-  background: url(@/assets/images/hk-banner.png) no-repeat, linear-gradient(#E53935, #E35D5B); 
-  background-size: 100%; 
+  width: 100vw; min-height: 100vh; background: url(@/assets/images/hk-banner.png) no-repeat, linear-gradient(#E53935, #E35D5B); background-size: 100%; 
 
   &-header {
     & h3 {
@@ -61,9 +73,32 @@ export default {
       }
     }
   }
+
+  &-qrcode {
+    display: flex; flex-direction: column; justify-content: center; align-items: center; margin: 20px 0;
+    &-img {
+      padding: 5px; border-radius: 5px; background: #fff;
+    }
+  }
+
   &-marquee{
-    & > li {
-      display: flex; flex: 1;
+    &-title {
+      margin-bottom: 15px;
+      & > h3 {
+        display: inline-block; width: 200px; height: 28px; background: url(@/assets/images/hk_title_bg.png) no-repeat; background-size: 100%; text-align: center; line-height: 28px;
+      }
+    }
+    ul {
+      padding: 15px; background: #fff; border-radius: 14px;
+      & > li {
+        display: flex; flex: 1; align-items: center;
+        .marquee-left {
+          display: flex; flex: 1; align-items: center; text-align: left;
+        }
+        .marquee-right {
+          text-align: right;
+        }
+      }
     }
   }
 }
